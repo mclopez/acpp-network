a1d660eb5a69e7a438d7b200ffbd856f0e6d11a3
marcos cambon <mclopez@users.noreply.github.com>
2025-10-06 19:08:45 +0200

    stream socket

--- include/acpp-network/socket.h
+++ include/acpp-network/socket.h
@@ -1,8 +1,48 @@
+//  Copyright Marcos Cambón-López 2025.
+
+// Distributed under the Mozilla Public License Version 2.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.mozilla.org/en-US/MPL/2.0/)
+
 #pragma once
 
-#include <sys/socket.h> 
+// #include <sys/socket.h> 
+// #include <netinet/in.h> // For sockaddr_in structure
+// #include <sys/un.h>    // For sockaddr_un structure
+// #include <netdb.h>
 
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <ws2def.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <windows.h>
+#include <io.h>
+
+using in_port_t = decltype(sockaddr_in::sin_port);
+
+#else
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/un.h>    // For sockaddr_un structure
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#endif
+
+
+#include <variant>
+#include <stdexcept>
 #include <string>
+#include <functional>
+#include <cstring>
+
+void log_error(const std::string& msg);
 
 namespace acpp::network {
 
@@ -12,13 +52,14 @@ template<> struct address<PF_INET>  { using type = struct in_addr; };
 template<> struct address<PF_INET6> { using type = struct in6_addr; };
 template<> struct address<PF_UNIX>  { using type = struct iun_addr; };
 
+
+
 class socket_base {
 public:    
-    socket_base(int domain, int type, int protocol):fd_(::socket(domain, type, protocol)){}
+    socket_base() = default;
+    socket_base(int fd):fd_(fd){}
     ~socket_base() {
-        if (fd_ != -1) {
-            ::close(fd_);
-        }
+        close();
     }
 
     socket_base(const socket_base&) = delete;
@@ -27,43 +68,170 @@ public:
     socket_base(socket_base&& other) noexcept;
     socket_base& operator=(socket_base&& other) noexcept;
 
+    void create_impl(int domain, int type, int protocol) {
+        if (valid())
+            close();
+        fd_ = ::socket(domain, type, protocol);
+    }
 
-    bool is_valid() const {return fd_ != -1;}
+    bool valid() const {return fd_ != invalid_fd;}
+
     int fd() const { return fd_; }
     void close() {
-        if (fd_ != -1) {
+        if (fd_ != invalid_fd) {
+#ifndef _WIN32            
             ::close(fd_);
-            fd_ = -1;
+#else            
+            ::shutdown(fd_, SD_SEND);
+            ::closesocket(fd_);
+#endif
+            fd_ = invalid_fd;
         }
     }
 
 private:
-    int fd_ = -1; 
+#ifdef _WIN32            
+    constexpr static int invalid_fd = INVALID_SOCKET;
+#else            
+    constexpr static int invalid_fd = -1;
+#endif
+    int fd_ = invalid_fd; 
 };
 
-template<int Fam, int Type, int Protocol = 0>
-class socket: socket_base {
+//using IpAddress = std::variant<struct in_addr, struct in6_addr>;
+//using IpAddress = std::variant<sockaddr_in, sockaddr_in6>;
+
+
+struct ip4_sockaddress {
+    ip4_sockaddress() = default;
+    ip4_sockaddress(const std::string& ip, in_port_t port) {  
+        addr.sin_family = AF_INET;                 // IPv4
+        //addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Connect to localhost
+        int r = inet_pton(AF_INET, ip.c_str(), &addr.sin_addr.s_addr);
+        if (r <= 0) {
+            throw std::invalid_argument("Invalid IP address format");
+        }
+        addr.sin_port = htons(port);               // Port to connect to (converted to network byte order)
+    }
+    int family() const { return addr.sin_family; }
+    std::string ip()const {
+        char ip_str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &addr.sin_addr, ip_str, sizeof(ip_str));
+        return std::string(ip_str); 
+    }
+    in_port_t port() const {            
+        return ntohs(addr.sin_port);
+    }   
+    sockaddr_in addr;
+};
+
+struct ip6_sockaddress {
+    ip6_sockaddress() = default;
+    ip6_sockaddress(const std::string& ip, in_port_t port) {  
+        std::memset(&addr, 0, sizeof(addr));
+        addr.sin6_family = AF_INET6;                 // IPv4
+        //addr.sin6_addr.s_addr = htonl(INADDR_LOOPBACK); // Connect to localhost
+
+        addr.sin6_flowinfo = 0;
+        addr.sin6_scope_id = 0;
+
+        int result = inet_pton(AF_INET6, ip.c_str(), &addr.sin6_addr);
+        if (result <= 0) {
+            throw std::invalid_argument("Invalid IPv6 address format"); 
+        }
+        addr.sin6_port = htons(port); 
+    }
+    int family() const { return addr.sin6_family; }
+    std::string ip() const {
+        char ip_str[INET6_ADDRSTRLEN];
+        inet_ntop(AF_INET6, &addr.sin6_addr, ip_str, sizeof(ip_str));
+        return std::string(ip_str); 
+    }
+    in_port_t port() const {            
+        return ntohs(addr.sin6_port);
+    }
+    sockaddr_in6 addr;
+};
+
+using ip_socketaddress = std::variant<ip4_sockaddress, ip6_sockaddress>;
+
+#ifndef _WIN32
+using un_socketaddress = std::variant<sockaddr_un>;
+int get_family(const un_socketaddress& addr);
+#endif
+
+int get_family(const ip_socketaddress& addr);
+
+void from_sockaddr(const sockaddr& sa, ip4_sockaddress& out_addr);
+void from_sockaddr(const sockaddr& sa, ip6_sockaddress& out_addr);
+void from_sockaddr(const sockaddr& sa, ip_socketaddress& out_addr);
+
+const sockaddr& to_sockaddr(const ip4_sockaddress& addr);
+const sockaddr& to_sockaddr(const ip6_sockaddress& addr);
+const sockaddr& to_sockaddr(const ip_socketaddress& addr);
+
+std::string to_string(const ip_socketaddress& addr);
+
+
+//const sockaddr& to_sockaddr(const ip_socketaddress& addr);
+
+template<typename Address, int Protocol = 0>
+class stream_socket {
 public:
-    constexpr static auto family = Fam;
-    constexpr static auto type = Type;
+    using address_type = Address;
+    constexpr static auto socket_type = SOCK_STREAM;
     constexpr static auto protocol = Protocol;
-    
-    using addres_type = address<type>::type;
 
-    socket():socket_base(family, type, protocol) {}
+    stream_socket() = default;
+    stream_socket(int fd);
 
-    ~socket()=default;
+    ~stream_socket()=default;
 
-    bool connect(const addres_type& ad);
+    bool connect(const address_type& ad);
     size_t send(const char* data, size_t len);
     size_t receive(char* buffer, size_t len);
 
+    int bind(const address_type& ad);
+    int listen(int backlog = 5);
+    stream_socket accept();  
+
+
 private:
- 
-    bool resolve_address(); 
+    socket_base socket_;
 };
 
-using tcp_socket = socket<PF_INET, SOCK_STREAM>;
-using udp_socket = socket<PF_INET, SOCK_DGRAM>;
+
+template<typename Address, int Protocol = 0>
+class datagram_socket {
+public:
+    using address_type = Address;
+    constexpr static auto socket_type = SOCK_DGRAM;
+    constexpr static auto protocol = Protocol;
+
+    datagram_socket() = default;
+    datagram_socket(int fd);
+
+    ~datagram_socket()=default;
+
+    //bool connect(const address_type& ad);
+    size_t send_to(const address_type& addr, const char* data, size_t len);
+    size_t recv_from(address_type& addr, char* buffer, size_t len );
+
+    int bind(const address_type& ad);
+
+private:
+    socket_base socket_;
+};
+
+using tpc_socket = stream_socket<ip_socketaddress>;
+using udp_socket = datagram_socket<ip_socketaddress>;
+
+template<typename SocketAddress>
+using resolve_address_callback = std::function<void(SocketAddress& addr, bool& success)>;
+
+//template<typename SocketAddress>
+//void resolve_host(const std::string& host, const std::string& service, resolve_address_callback<SocketAddress>&& callback);
+template<typename Socket, typename Address = Socket::address_type>
+void resolve_host(const std::string& host, const std::string& service, resolve_address_callback<Address>&& callback);
 
 } // namespace acpp::network 
--- src/socket.cpp
+++ src/socket.cpp
@@ -1,28 +1,117 @@
+//  Copyright Marcos Cambón-López 2025.
 
-#include <netdb.h>
-#include <unistd.h>
+// Distributed under the Mozilla Public License Version 2.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.mozilla.org/en-US/MPL/2.0/)
+
+//#include <netdb.h>
+//#include <unistd.h>
 #include <cstring>
 #include <iostream>
 
 #include <acpp-network/socket.h>
 
+#include <iostream>
+#include <string>
 
-namespace {
-    void log_error(const std::string& func) {
-        std::cerr << "[POSIX] Error in " << func << ": " << strerror(errno) << std::endl;
+#ifdef _WIN32
+#include <windows.h> // Required for WSAGetLastError, FormatMessage, etc.
+#endif
+
+
+
+
+
+
+#ifdef _WIN32
+
+// Function to convert a Winsock error code to a string
+std::string winsock_error_to_string(int errorCode) {
+    // Flag to format message from the system's message table
+    // and allocate memory for the string buffer.
+    const DWORD flags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER;
+    
+    // Pass the error code as the message identifier.
+    // Use the language neutral ID (LANGID 0).
+    LPTSTR buffer = nullptr;
+    DWORD length = FormatMessage(
+        flags,
+        NULL,              // Source (not used when FORMAT_MESSAGE_FROM_SYSTEM)
+        errorCode,         // The error code
+        0,                 // Language ID (0 for language-neutral)
+        (LPTSTR)&buffer,   // Output buffer pointer
+        0,                 // Minimum buffer size
+        NULL               // Arguments (not used)
+    );
+
+    if (length == 0) {
+        // If FormatMessage failed, return a default message with the code.
+        return "Winsock Error " + std::to_string(errorCode) + " (No description available)";
     }
+
+    // The returned buffer contains the string (which may have a trailing newline).
+    std::string message(buffer);
+
+    // Free the buffer allocated by FormatMessage
+    LocalFree(buffer); 
+
+    // Windows error messages often include a trailing newline/carriage return. Remove it.
+    size_t last_char = message.find_last_not_of("\r\n");
+    if (std::string::npos != last_char) {
+        message.resize(last_char + 1);
+    }
+    
+    return message;
 }
+
+
+class socket_init{
+public:    
+    socket_init();
+    ~socket_init();
+};
+
+
+socket_init::socket_init(){
+    std::cout << "socket_init" << std::endl;
+    WSADATA wsaData;
+    // We request Winsock version 2.2 (the standard modern version)
+    WORD wVersionRequested = MAKEWORD(2, 2); 
+
+    // 2. Initialize Winsock
+    int iResult = WSAStartup(wVersionRequested, &wsaData);
+    
+    if (iResult != 0) {
+        // Initialization failed. Print the error code.
+        // (WSAGetLastError() won't work reliably here, as the environment isn't set up yet)
+        std::cerr << "WSAStartup failed with error: " << iResult << std::endl;
+    }
+    //TODO: throw exception??
+}
+
+socket_init::~socket_init(){
+    std::cout << "~socket_init" << std::endl;
+    //TODO: always even when WSAStartup fails??
+    WSACleanup();
+}
+
+socket_init init;
+
+#endif
+
+void log_error(const std::string& func) {
+    std::string error;
+#ifndef _WIN32
+    error = strerror(errno);
+#else
+    error = winsock_error_to_string(WSAGetLastError());
+#endif
+    std::cerr << "[POSIX] Error in " << func << ": " << error << std::endl;
+}
+
 
 namespace acpp::network {
 
-// socket::socket(const std::string& hostname, const std::string& port) {
-//     fd_ = socket(AF_INET, SOCK_STREAM, 0);
-//     if (fd_ < 0) {
-//         log_error("socket");
-//     } else {
-//         std::cout << "Socket created with FD: " << fd_ << std::endl;
-//     }
-// }
 
 
 socket_base::socket_base(socket_base&& other) noexcept 
@@ -38,6 +127,90 @@ socket_base& socket_base::operator=(socket_base&& other) noexcept {
     }
     return *this;
 }
+
+int get_family(const ip_socketaddress& addr) {
+    return std::visit([](auto&& arg) -> int {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, ip4_sockaddress>) {
+            return AF_INET;
+        } else if constexpr (std::is_same_v<T, ip6_sockaddress>) {
+            return AF_INET6;
+        } else {
+            return AF_UNSPEC;
+            //throw std::invalid_argument("Unknown address type");
+        }
+    }, addr);
+}
+#ifndef _WIN32
+int get_family(const un_socketaddress& addr) {
+    return AF_UNIX;
+}
+#endif
+
+std::string to_string(const ip_socketaddress& addr) {
+    return std::visit([](auto&& arg) -> std::string {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, ip4_sockaddress>) {
+            return std::string(arg.ip() + ":" + std::to_string(arg.port()));
+        } else if constexpr (std::is_same_v<T, ip6_sockaddress>) {
+            return std::string(arg.ip() + ":" + std::to_string(arg.port()));
+        } else {
+            throw std::invalid_argument("Unknown address type");
+        }
+    }, addr);
+}
+/*
+const sockaddr& to_sockaddr(const ip_socketaddress& addr) {
+    // return std::visit([](auto&& arg) -> sockaddr {
+    //     using T = std::decay_t<decltype(arg)>;
+    //     if constexpr (std::is_same_v<T, struct in_addr>) {
+    //         return *reinterpret_cast<sockaddr*>(&sa);
+    //     } else if constexpr (std::is_same_v<T, struct in6_addr>) {
+    //         return *reinterpret_cast<sockaddr*>(&sa);
+    //     } else {
+    //         throw std::invalid_argument("Unknown address type");
+    //     }
+    // }, addr);
+    //TODO: better way??
+    return reinterpret_cast<const sockaddr&>(addr);
+}
+*/
+
+
+const sockaddr& to_sockaddr(const ip4_sockaddress& addr) {
+    return (const sockaddr &)addr.addr;
+}
+const sockaddr& to_sockaddr(const ip6_sockaddress& addr) {
+    return (const sockaddr&)addr.addr;
+}
+/*
+const sockaddr& to_sockaddr(const ip_socketaddress& addr)  {
+    return std::visit([](auto&& arg) -> const sockaddr& {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, ip4_sockaddress>) {
+            return to_sockaddr(std::get<ip4_sockaddress>(arg));
+        }
+        else if constexpr (std::is_same_v<T, ip6_sockaddress>) {
+            return to_sockaddr(std::get<ip6_sockaddress>(arg));
+        }
+        else {
+            //return AF_UNSPEC;
+            throw std::invalid_argument("to_sockaddr: Unknown address type");
+        }
+        }, addr);
+}
+*/
+
+const sockaddr& to_sockaddr(const ip_socketaddress& addr)  {
+    if(const ip4_sockaddress* v = std::get_if<ip4_sockaddress>(&addr)) {
+        return to_sockaddr(*v);
+    } else if(const ip6_sockaddress* v = std::get_if<ip6_sockaddress>(&addr)) {
+        return to_sockaddr(*v);
+    } else {
+        throw std::invalid_argument("to_sockaddr: Unknown address type");
+    }
+}
+
 
 
 } //namespace acpp::network 
